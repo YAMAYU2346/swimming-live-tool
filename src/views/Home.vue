a614ab6ab3e571a2c354fc20a536d7cdcc03622c
YAMAYU2346 <yuya350@gmail.com>
2021-05-24 17:46:44 +0900

    Home画面を修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <time-table-admin />
+  <v-container>
+    <v-row dense>
+      <v-col cols="12">
+        <v-card to='/time-table'>
+          <v-card-title class="text-h5">
+            <v-icon>mdi-calendar</v-icon>Time Table
+          </v-card-title>
+          <v-card-subtitle>
+            試合の競技一覧を取得・選択し、OBSのテロップに反映させます
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import TimeTableAdmin from '../components/TimeTableAdmin.vue'
 
 export default Vue.extend({
   name: 'Home',
 
-  components: {
-    TimeTableAdmin
-  }
+  components: {}
 })
 </script>
